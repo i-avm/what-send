24c00f2bfc71f2418b3e9b589dff5b6807023a76
avm <abyvarghese333@gmail.com>
2023-03-04 21:18:56 +0530

    fix: ts failing with unidentified module error

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,19 +7,20 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <>
-      <Head>
-        <title>WhatSend</title>
-        <meta name="description" content="Send WhatsApp Messages to numbers without saving into contacts" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main>
-        <NoSSR>
-          <ContactForm />
-        </NoSSR>
+    <NoSSR>
 
-      </main>
-    </>
+      <>
+        <Head>
+          <title>WhatSend</title>
+          <meta name="description" content="Send WhatsApp Messages to numbers without saving into contacts" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <main>
+          <ContactForm />
+        </main>
+      </>
+    </NoSSR>
+
   )
 }
