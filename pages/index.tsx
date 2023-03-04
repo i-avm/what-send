20bfb4d0fc78c03f892b285b0d5b355aa5cee1d0
avm <abyvarghese333@gmail.com>
2023-03-04 21:02:04 +0530

    Init commit

---
+++ pages/index.tsx
@@ -0,0 +1,25 @@
+import Head from 'next/head'
+import ContactForm from './contact-form'
+import { Inter } from 'next/font/google'
+import NoSSR from 'react-no-ssr';
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>WhatSend</title>
+        <meta name="description" content="Send WhatsApp Messages to numbers without saving into contacts" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <NoSSR>
+          <ContactForm />
+        </NoSSR>
+
+      </main>
+    </>
+  )
+}
